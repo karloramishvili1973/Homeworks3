3cdd1a3038d03eb5723ee8f45ad7afd2fb7a3991
unknown <karloramishvili@gmail.com>
2022-11-30 23:04:47 +0400

    Visual studio changes

---
+++ Homeworks/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homeworks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("test");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Rendom text");
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+        }
+    }
+}
